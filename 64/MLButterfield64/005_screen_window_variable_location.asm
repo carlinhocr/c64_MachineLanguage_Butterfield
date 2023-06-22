7547ee5c8ac599b462b9201b33d7ef9573046b0b
carlinhocr <carlos.rienzi@gmail.com>
2023-06-22 15:41:37 -0300

    chap 5 examples

--- 64/MLButterfield64/005_screen_window_variable_location.asm
+++ 64/MLButterfield64/005_screen_window_variable_location.asm
@@ -1,20 +1,17 @@
 ; manage a window in the c64
 ;all address and mappings according to c64
-startcolumn=$06
-endcolumn=$1a
-startrow=$05 ;this is row 5 we will multiply by 40 to abtain row
-endrow=$0e
+startcolumn=$04
 *=$033c
         lda #$28 ; load 40 for the number of columns on the c64
         ldx $0288 ; address for start of screen location
         sta $03a0 ; store the line lenght
-        lda #startrow*40        ;startrow we multiply by 40 amount of characters
+        lda #$00
         sta $bb         ;store the low byte of where screen starts
         stx $bc         ;store the high byte of where screen starts
         ;start counting screen lines
         ldx #$00
 NEXTLINE
-        ldy #startcolumn        ;to start at column 5 put an index of 4 on Y
+        ldy #$04        ;to start at column 5 put an index of 4 on Y
 NEXTCHAR
         lda ($bb),y     ;load the character currenlty on screen
         cmp #$20        ;compare to space character
@@ -24,7 +21,7 @@ ISSPACE
         sta ($bb),y     ;store modified character back into screen
         ;go to next character if it us column 18 go to next line
         iny
-        cpy #endcolumn        ;18 in decimal
+        cpy #$12        ;18 in decimal
         bcc NEXTCHAR    ;if it is less than 18
         ;add 40 to the start of screen memory address to start next line
         clc     ;carry clear prior to addition
@@ -36,7 +33,7 @@ ISSPACE
         sta $bc         ;store high byte
         ;lets count the lines
         inx
-        cpx #endrow        ;compare to 14
+        cpx #$0e        ;compare to 14
         bne NEXTLINE
         rts
         
